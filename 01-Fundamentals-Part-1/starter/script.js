e93e6562dc35ebc76e4d1bde4304e93a32c726df
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-09-09 09:06:15 +0100

    workig with strings

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,15 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
+// if (js === 'amazing') alert('Javascript is FUN!');
 
 console.log(40 + 28 + 3 - 10);
+
+//LECTURE: Values and Variables
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = '220 Million';
+
+console.log('Country:', country);
+console.log('Continent:', continent);
+console.log('Population:', population);
+
+
